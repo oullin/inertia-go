1858ee686f067fea0f4c6b610761af5d8d534c06
Gustavo Ocanto <gustavoocanto@gmail.com>
2026-04-01 16:19:04 +0800

    js config

--- example/app/vite.config.js
+++ example/app/vite.config.js
@@ -1,9 +1,15 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   root: ".",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
